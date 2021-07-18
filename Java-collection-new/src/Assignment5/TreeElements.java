f8ab80ea35e6039910e65442ced63e54e8492826
ASH1815 <kumawatashok1815@gmail.com>
2021-07-18 15:13:37 +0530

    :v:Question-3-hashset

---
+++ Java-collection-new/src/Assignment5/TreeElements.java
@@ -0,0 +1,40 @@
+package Assignment5;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeElements {
+
+    public  static void main(String[] args)
+    {
+        TreeSet<String> name =new TreeSet<>();
+        name.add("ASHOK");
+        name.add("SUNIL");
+        name.add("KUMAR");
+
+        String query="ASHOK";
+
+        boolean flag=false;
+
+        Iterator<String > it= name.iterator();
+
+        while(it.hasNext())
+        {
+            if(it.next().equals(query))
+            {
+                flag=true;
+                break;
+            }
+        }
+
+        if(flag)
+        {
+            System.out.println(query +" present in  the tree set");
+        }
+        else
+        {
+            System.out.println(query +" not present in the tree set");
+        }
+
+    }
+}
